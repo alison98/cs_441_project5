590bedc8321ddbe588fd76851f831142d61a344c
alison98 <nosila98@gmail.com>
2021-03-19 10:21:58 -0400

    follow tutorial

--- project5/app/src/main/java/com/example/project5/MainActivity.java
+++ project5/app/src/main/java/com/example/project5/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.project5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    SensorManager manager;
+    Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if(sensor == null) {
+            finish();
+        }
+        manager.registerListener(sensorListener, sensor, 2 * 1000 * 1000);
     }
+
+    SensorEventListener sensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            if(sensorEvent.values[2] > 0.5f) { // anticlockwise
+                getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+            } else if(sensorEvent.values[2] < -0.5f) { // clockwise
+                getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+        }
+    };
 }
